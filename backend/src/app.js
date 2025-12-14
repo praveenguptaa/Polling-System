32ebcabe26f31c5f13a4dca9f698f85490916455
Praveen Gupta <praveenguptaa8@gmail.com>
2025-12-14 15:40:33 +0530

    origin changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173"
+      "https://polling-system3.onrender.com"
     ],
     methods: ["GET", "POST"],
     credentials: true,
